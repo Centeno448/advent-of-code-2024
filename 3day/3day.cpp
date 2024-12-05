f6036a4b4330b6174d7c139ec0d0af1a349b0dc6
Centeno448 <diegocenteno448@gmail.com>
2024-12-04 22:21:46 -0500

    day 3-2 solution

--- 3day/3day.cpp
+++ 3day/3day.cpp
@@ -31,16 +31,44 @@ int main(int argc, char *argv[])
     int res = 0, res2 = 0;
 
     std::regex mul_regex("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    std::regex mul_regex2("mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))|(don't\\(\\))");
 
     while (getline(in, line))
     {
+        std::string copy = line;
         smatch mul_match;
-        while (regex_search(line, mul_match, mul_regex))
+        while (regex_search(copy, mul_match, mul_regex))
         {
             int left = stoi(mul_match[1]);
             int right = stoi(mul_match[2]);
             res += left * right;
-            line = mul_match.suffix();
+            copy = mul_match.suffix();
+        }
+
+        std::string copy2 = line;
+        smatch mul_match2;
+        bool process = true;
+        while (regex_search(copy2, mul_match2, mul_regex2))
+        {
+            if (mul_match2[0].str() == "do()")
+            {
+                process = true;
+            }
+            else if (mul_match2[0].str() == "don't()")
+            {
+                process = false;
+            }
+            else
+            {
+                if (process)
+                {
+                    int left = stoi(mul_match2[1]);
+                    int right = stoi(mul_match2[2]);
+                    res2 += left * right;
+                }
+            }
+
+            copy2 = mul_match2.suffix();
         }
     }
 
